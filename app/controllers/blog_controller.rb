f0b4f5dd64c0f64d6a02576933226a4ccd1b86ab
Cliff Zhao <zty@Cliffs.local>
2014-04-29 13:57:28 +0800

    finish bloglist pagination, and add no blog notice

--- app/controllers/blog_controller.rb
+++ app/controllers/blog_controller.rb
@@ -1,12 +1,17 @@
 class BlogController < ApplicationController
+  BlogPerPage = 5
+
   def listbyuser
     @user = User.find(params[:id])
     @username = @user.username
     @title = @username + "'s blog"
     @blog_title = @username + ' の ブログ'
 
-    @page_count = (@user.blog_articles.size / 5.0).ceil
-    @current_page = Integer(params[:page] || 1)
-    @articles = @user.blog_articles.offset((@current_page - 1) * 5).limit(5)
+    @has_articles = @user.blog_articles.size > 0
+    if @has_articles
+      @page_count = (@user.blog_articles.size / Float(BlogPerPage)).ceil
+      @current_page = Integer(params[:page] || 1)
+      @articles = @user.blog_articles.offset((@current_page - 1) * BlogPerPage).limit(BlogPerPage)  
+    end
   end
 end
