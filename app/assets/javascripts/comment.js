4eb80ff11b1b65c816e75e8a8b818fadcab4516c
Cliff Zhao <zty@Cliffs.local>
2014-05-04 16:31:25 +0800

    comment pagination finish

--- app/assets/javascripts/comment.js
+++ app/assets/javascripts/comment.js
@@ -1,6 +1,48 @@
+function generateLiLinkCode (linkto, text) {
+  return [
+    '<li><a href="javascript:void(0)" data-page="',
+    String(linkto),
+    '">',
+    String(text),
+    '</a></li>'
+  ].join('');
+}
+
+function generatePageLiSetCode (currentPage, pageCount) {
+  var liSetArr = [];
+  if (currentPage == 1) {
+    liSetArr.push([
+      '<li>前へ</li>',
+      '<li class="current-page">1</li>'
+      ].join(''));
+  } else {
+    liSetArr.push(generateLiLinkCode(currentPage - 1, '前へ'));
+    liSetArr.push(generateLiLinkCode(1, 1));
+  }
+  for (var i = 2 ; i <= pageCount ; i ++) {
+    if (i == currentPage) {
+      liSetArr.push('<li class="current-page">' + i + '</li>');
+    } else {
+      liSetArr.push(generateLiLinkCode(i, i));
+    }
+  }
+  if (currentPage == pageCount) {
+    liSetArr.push([
+      '<li>次へ</li>',
+      '<li>合計' + pageCount + 'ページ</li>'
+      ].join(''));
+  } else {
+    liSetArr.push(generateLiLinkCode(parseInt(currentPage) + 1, '次へ'));
+    liSetArr.push('<li>合計' + pageCount + 'ページ</li>');
+  }
+  return liSetArr.join('');
+}
+
 $('document').ready(function () {
   var articleId = $('.comment-area-wrapper').attr('data-article-id'),
-    commentTempl = [
+    commentArea = $('.blog-bottom-wrapper').get(0);
+  if (! articleId) return;
+  var commentTempl = [
       '{{#comments}}',
       '<li class="comment-item">',
       '<a class="user-small-logo" href="/blog/listbyuser?id={{arid}}">',
@@ -18,7 +60,27 @@ $('document').ready(function () {
   }, function (data) {
     var comments = data.comments;
     if (comments.length > 0) {
-      $('.comment-list').append(Mustache.render(commentTempl, data));
+      var commentList = $('.comment-list'),
+        commentPagination = $('.comment-pagination');
+      commentList.append(Mustache.render(commentTempl, data));
+      var pageCount = data.page_count;
+      commentPagination
+        .append(generatePageLiSetCode(1, pageCount))
+        .delegate('li a', 'click', function (e) {
+          var nextPage = $(e.currentTarget).attr('data-page');
+          $.get('/comment/getcomments', {
+            'id': articleId,
+            'page': nextPage
+          }, function (data) {
+            commentList
+              .empty()
+              .append(Mustache.render(commentTempl, data));
+            commentPagination
+              .empty()
+              .append(generatePageLiSetCode(nextPage, data.page_count));
+            commentArea.scrollIntoView();
+          });
+        });
     }
   });
 });
