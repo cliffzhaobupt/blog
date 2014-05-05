b5e4201565475fab75ca661363a6d76151b9790d
Cliff Zhao <zty@Cliffs.local>
2014-05-05 16:29:40 +0800

    add gems to Gemfile

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,7 +1,16 @@
+require 'mini_magick'
+
 class User < ActiveRecord::Base
     has_many :blog_articles
     has_many :tags
 
     validates :username, uniqueness: true
     validates :username, :password, :email, :gender, presence: true
+
+    def upload_photo= picture_field
+        image = MiniMagick::Image.read(picture_field.read)
+        image.resize '200x200'
+        image.format 'png'
+        self.photo = image.to_blob
+    end
 end
